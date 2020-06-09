4476be7b2a8771626c3cae99815c68beb273cc2e
george sedky <kajogo777@gmail.com>
2020-06-09 07:09:46 +0200

    Some deployment changes

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,11 +2,10 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/georgesedky/Development/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/georgesedky/Development/EvangelionMobileApp"
-export "FLUTTER_TARGET=/Users/georgesedky/Development/EvangelionMobileApp/lib/main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=/Users/georgesedky/Development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/georgesedky/Development/flutter/bin/cache/artifacts/engine/ios-release"
 export "FLUTTER_BUILD_NAME=0.1.6"
 export "FLUTTER_BUILD_NUMBER=8"
-export "TRACK_WIDGET_CREATION=true"
